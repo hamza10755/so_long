5c04d8c3d88991cb975232564fbea2f8c525d64a
hamza10755 <202311944@students.asu.edu.jo>
2025-02-12 23:05:10 +0300

    Wednesday check

--- src/map.c
+++ src/map.c
@@ -3,29 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbelaih <hbelaih@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hamzabillah <hamzabillah@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 13:12:05 by hbelaih           #+#    #+#             */
-/*   Updated: 2025/02/11 17:00:08 by hbelaih          ###   ########.fr       */
+/*   Updated: 2025/02/12 23:04:18 by hamzabillah      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int	get_map_dimensions(char *file, t_game *game)
+#include "so_long.h"
+
+static int	close_fd(int fd, int ret)
 {
-	int		fd;
+	if (fd >= 0)
+		close(fd);
+	return (ret);
+}
+
+static int	set_map_dimensions(int fd, t_game *game)
+{
 	char	*line;
 
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (0);
-	line = get_next_line(fd);//check this shit
+	line = get_next_line(fd);
 	if (!line)
-	{
-		close(fd);
 		return (0);
-	}
 	game->map->width = ft_strlen(line);
 	if (line[game->map->width - 1] == '\n')
 		game->map->width--;
@@ -36,56 +38,40 @@ static int	get_map_dimensions(char *file, t_game *game)
 		free(line);
 		line = get_next_line(fd);
 	}
-	close(fd);
 	return (1);
 }
-int	fd_ftaa7(t_game *game, char *file)
+
+int	get_map_dimensions(char *file, t_game *game)
 {
 	int	fd;
 
-	if (!get_map_dimensions(file, game))
-		return (0);
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
-		exit(1);
-	return (fd);
+		return (0);
+	if (!set_map_dimensions(fd, game))
+		return (close_fd(fd, 0));
+	return (close_fd(fd, 1));
 }
+
 int	read_map(t_game *game, char *file)
 {
 	int		fd;
 	char	*line;
 	int		i;
 
-	i = 0;
-	fd = fd_ftaa7(game, file);
-	line = get_next_line(fd);
-	if (!line)
-	{
-		close(fd);
-		exit_game(game);
-	}
-	// game->map->array = malloc(sizeof(char *) * (game->map->height + 1));
-	game->map->array = NULL;
-	if (!game->map->array)
-	{
-		close(fd);
-		free(line);
-		line = get_next_line(fd);
-		while (line)
-		{
-			free(line);
-			line = get_next_line(fd);
-		}
+	fd = open(file, O_RDONLY);
+	if (fd < 0 || !get_map_dimensions(file, game))
 		return (0);
-	}
-	while (line)
+	game->map->array = malloc(sizeof(char *) * (game->map->height + 1));
+	if (!game->map->array)
+		return (close_fd(fd, 0));
+	i = 0;
+	while ((line = get_next_line(fd)))
 	{
 		if (line[ft_strlen(line) - 1] == '\n')
 			line[ft_strlen(line) - 1] = '\0';
 		game->map->array[i++] = line;
-		line = get_next_line(fd);
 	}
-	game->map->array[i] = NULL; // game->mommy = 0;
-	close(fd);
-	return (1);
+	game->map->array[i] = NULL;
+	return (close_fd(fd, 1));
 }
